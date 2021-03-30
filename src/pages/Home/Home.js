71be8997dae126f9161a8705f5d73afb41168ff0
MarcellCole <mcoleman@icstars.org>
2021-03-30 09:12:38 -0500

    submit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Home.css'
+import Logo from '../../components/Logo/Logo';
+import Button from '../../components/Button/Button';
+import Card from '../../components/Card/Card';
+import {Grid} from '@material-ui/core';
+import Accordion from '../../components/Accordion/Accordion';
+import Inbox from '../../components/Inbox/Inbox';
+import ProfilePic from '../../components/ProfilePic/ProfilePic';
+
+function Home(){
+    const [firstNames, setFirstNames] = React.useState(['henry','jason','alexa','khalil']);
+    const [currentUser, setCurrentUser] = React.useState('Brandon')
+
+    const changefirstName = ()=> {
+        setCurrentUser('Jason')
+    }
+
+    return(
+        <Grid container spacing={3}>
+            <Button buttonName="Back" color="blue" onClick={changefirstName}/>
+            <Button buttonName="Play" color="pink"/>
+            <Button buttonName="Pause" color="black"/>
+            <Button buttonName="Next" color="red"/>
+            {/* <Logo/> */}
+            <Card titleName={currentUser} date="September 15, 1999" bodyText="loream fhewuahfowehifuiwef jfewuifh iuweefwh" image="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"/>
+            <Grid item xs={3}>
+                <Accordion title="react" body="fuwheaiufhuiewhuifhweofhguiywehio"/>
+                <Accordion title="JS" body="fjowefwe"/>
+                <Accordion title="HTML" body="for noobs"/>
+                <Accordion title="CSS" body="not today"/>
+                <Accordion title="AWS" body="cloud computing"/>
+                <Inbox firstName='Marcell' lastName='Coleman' nickName='Beastymc' dateOfBirth='June 3rd, 1996' />
+                <ProfilePic small='../../pics/Sasuke' regular='../../pics/Rem' large='../../pics/Kaisen' />
+            </Grid>
+
+            {/* looks through the firstNames state and for each value inside the array, maps out a button with the name of the values inside of state. */}
+            {firstNames.map(item=>
+                <div>
+                    <Button buttonName={item}/>
+                </div>
+            )}
+        </Grid>
+    )
+}
+
+export default Home;
